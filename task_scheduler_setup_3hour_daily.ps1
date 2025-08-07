fa8724f6d732499dcf94cc578f9eb895f0f916ae
Jeff Brausam <48197040+Jeff-Brausam@users.noreply.github.com>
2025-08-07 14:03:12 -0700

    Add files via upload

---
+++ task_scheduler_setup_3hour_daily.ps1
@@ -0,0 +1,6 @@
+﻿$PathToScript = ""
+$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-NoProfile -ExecutionPolicy Bypass -File $PathToScript.ps1"
+$trigger = New-ScheduledTaskTrigger -Daily -At "00:00" -RepetitionInterval (New-TimeSpan -Hours 3) -RepetitionDuration (New-TimeSpan -Days 1)    
+$principal = New-ScheduledTaskPrincipal -UserId "YOUR_USERNAME" -LogonType Password -RunLevel Highest
+$settings = New-ScheduledTaskSettingsSet -Hidden -StartWhenAvailable
+Register-ScheduledTask -TaskName "MyNewTask" -Action $action -Trigger $trigger -Settings $settings -Principal $principal
