de0d2109c3430c4884328563c710ef275a363f30
Jeff Brausam <48197040+Jeff-Brausam@users.noreply.github.com>
2025-08-07 14:01:51 -0700

    Delete task_scheduler_setup_3hour_daily.ps1

--- task_scheduler_setup_3hour_daily.ps1
+++
@@ -1,6 +0,0 @@
-﻿$PathToScript = ""
-$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-NoProfile -ExecutionPolicy Bypass -File $PathToScript.ps1"
-$trigger = New-ScheduledTaskTrigger -Daily -At "00:00" -RepetitionInterval (New-TimeSpan -Hours 3) -RepetitionDuration (New-TimeSpan -Days 1)    
-$principal = New-ScheduledTaskPrincipal -UserId "YOUR_USERNAME" -LogonType Interactive -RunLevel Highest
-# $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries
-Register-ScheduledTask -TaskName "MyNewTask" -Action $action -Trigger $trigger -Principal $principal
